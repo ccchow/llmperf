89259e98110a105de6b3ae483181b654e2c109c4
ccchow <zhou.lei@outlook.com>
2024-08-30 18:19:51 -0700

    more

--- src/llmperf/ray_clients/trtllm_client.py
+++ src/llmperf/ray_clients/trtllm_client.py
@@ -12,6 +12,8 @@ import ray
 import requests
 from transformers import LlamaTokenizerFast
 
+import torch
+
 from llmperf.ray_llm_client import LLMClient
 from llmperf.models import RequestConfig
 from llmperf import common_metrics
@@ -23,6 +25,9 @@ class TrtLLMClient(LLMClient):
     runner = None
     tokenizer = None
 
+    engine_dir = None
+    max_output_len = 2048
+
     def __init__(self):
         self.tokenizer = LlamaTokenizerFast.from_pretrained(
             "hf-internal-testing/llama-tokenizer"
@@ -31,15 +36,12 @@ class TrtLLMClient(LLMClient):
         runtime_rank = tensorrt_llm.mpi_rank()
         runner_cls = ModelRunnerCpp
         runner_kwargs = dict(
-            engine_dir=args.engine_dir,
-            lora_dir=args.lora_dir,
+            engine_dir=self.engine_dir,
             rank=runtime_rank,
-            debug_mode=args.debug_mode,
-            lora_ckpt_source=args.lora_ckpt_source,
-            max_output_len=args.max_output_len,
+            max_output_len=self.max_output_len,
         )
 
-        runner = runner_cls.from_dir(**runner_kwargs)
+        self.runner = runner_cls.from_dir(**runner_kwargs)
 
     def llm_request(self, request_config: RequestConfig) -> Dict[str, Any]:
         prompt = request_config.prompt
@@ -56,10 +58,15 @@ class TrtLLMClient(LLMClient):
 
         start_time = time.monotonic()
 
+        batch_input_ids = parse_input(tokenizer=self.tokenizer,
+                                    input_text=request_config.prompt,
+                                    add_special_tokens=False,
+                                    max_input_length=2048)
+
         with torch.no_grad():
-            outputs = runner.generate(
+            outputs = self.runner.generate(
                 batch_input_ids,
-                max_new_tokens=args.max_output_len,
+                max_new_tokens=2048,
                 max_attention_window_size=args.max_attention_window_size,
                 end_id=end_id,
                 pad_id=pad_id,
@@ -132,34 +139,74 @@ class TrtLLMClient(LLMClient):
         metrics[common_metrics.NUM_OUTPUT_TOKENS] = tokens_received
         metrics[common_metrics.NUM_INPUT_TOKENS] = prompt_len
 
-        with torch.no_grad():
-            tensorrt_llm_llama.setup(
-                batch_size=args.batch_size,
-                max_context_length=max_length,
-                max_new_tokens=args.output_len,
-                beam_width=args.num_beams,
-                max_attention_window_size=args.max_attention_window_size,
-                multi_block_mode=args.multi_block_mode,
-                enable_context_fmha_fp32_acc=args.enable_context_fmha_fp32_acc)
-            logger.info(f"Generation session set up with the parameters: \
-                batch_size: {tensorrt_llm_llama.batch_size}, \
-                max_context_length: {tensorrt_llm_llama.max_context_length}, \
-                max_new_tokens: {tensorrt_llm_llama.max_new_tokens}, \
-                beam_width: {tensorrt_llm_llama.beam_width}, \
-                max_attention_window_size: {tensorrt_llm_llama.max_attention_window_size}, \
-                multi_block_mode: {tensorrt_llm_llama.multi_block_mode}, \
-                enable_context_fmha_fp32_acc: {tensorrt_llm_llama.enable_context_fmha_fp32_acc}"
-                        )
-
-            if tensorrt_llm_llama.remove_input_padding:
-                output_ids = tensorrt_llm_llama.decode_batch(
-                    line_encoded, sampling_config)
-            else:
-                output_ids = tensorrt_llm_llama.decode(
-                    line_encoded,
-                    input_lengths,
-                    sampling_config,
-                )
-                torch.cuda.synchronize()
-
         return metrics, generated_text, request_config
+
+def parse_input(tokenizer,
+                input_text=None,
+                prompt_template=None,
+                input_file=None,
+                add_special_tokens=True,
+                max_input_length=923,
+                pad_id=None,
+                num_prepend_vtokens=[],
+                model_name=None,
+                model_version=None):
+    if pad_id is None:
+        pad_id = tokenizer.pad_token_id
+
+    batch_input_ids = []
+    if input_file is None:
+        if 'whisper' in model_name.lower():
+            batch_input_ids.append(tokenizer.prefix_tokens)
+        else:
+            for curr_text in input_text:
+                if prompt_template is not None:
+                    curr_text = prompt_template.format(input_text=curr_text)
+                input_ids = tokenizer.encode(
+                    curr_text,
+                    add_special_tokens=add_special_tokens,
+                    truncation=True,
+                    max_length=max_input_length)
+                batch_input_ids.append(input_ids)
+    else:
+        if input_file.endswith('.csv'):
+            with open(input_file, 'r') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                for line in csv_reader:
+                    input_ids = np.array(line, dtype='int32')
+                    batch_input_ids.append(input_ids[-max_input_length:])
+        elif input_file.endswith('.npy'):
+            inputs = np.load(input_file)
+            for row in inputs:
+                input_ids = row[row != pad_id]
+                batch_input_ids.append(input_ids[-max_input_length:])
+        elif input_file.endswith('.txt'):
+            with open(input_file, 'r', encoding='utf-8',
+                      errors='replace') as txt_file:
+                input_text = txt_file.readlines()
+                batch_input_ids = tokenizer(
+                    input_text,
+                    add_special_tokens=add_special_tokens,
+                    truncation=True,
+                    max_length=max_input_length)["input_ids"]
+        else:
+            print('Input file format not supported.')
+            raise SystemExit
+
+    if num_prepend_vtokens:
+        assert len(num_prepend_vtokens) == len(batch_input_ids)
+        base_vocab_size = tokenizer.vocab_size - len(
+            tokenizer.special_tokens_map.get('additional_special_tokens', []))
+        for i, length in enumerate(num_prepend_vtokens):
+            batch_input_ids[i] = list(
+                range(base_vocab_size,
+                      base_vocab_size + length)) + batch_input_ids[i]
+
+    if input_file is None and 'GLM' in model_name and model_version == 'glm':
+        for ids in batch_input_ids:
+            ids.append(tokenizer.sop_token_id)
+
+    batch_input_ids = [
+        torch.tensor(x, dtype=torch.int32) for x in batch_input_ids
+    ]
+    return batch_input_ids
