08d4d542cb0a50906b7d4e41611b6cd43aac5089
ccchow <zhou.lei@outlook.com>
2024-08-31 19:46:20 -0700

    done

--- src/llmperf/ray_clients/trtllm_client.py
+++ src/llmperf/ray_clients/trtllm_client.py
@@ -1,18 +1,13 @@
-import json
-import os
 import time
 from typing import Any, Dict
 
 import tensorrt_llm
 import tensorrt_llm.profiler
-from tensorrt_llm.logger import logger
-from tensorrt_llm.runtime import PYTHON_BINDINGS, ModelRunner, ModelRunnerCpp
-
+from tensorrt_llm.runtime import ModelRunnerCpp
 import numpy as np
 import csv
 
 import ray
-import requests
 from transformers import LlamaTokenizerFast
 
 import torch
@@ -50,12 +45,11 @@ class TrtLLMClient(LLMClient):
         prompt = request_config.prompt
         prompt, prompt_len = prompt
 
+        metrics = {}
         time_to_next_token = []
         tokens_received = 0
         ttft = 0
-        error_response_code = None
         generated_text = ""
-        error_msg = ""
         output_throughput = 0
         total_request_time = 0
 
@@ -66,67 +60,18 @@ class TrtLLMClient(LLMClient):
                                     add_special_tokens=False,
                                     max_input_length=2048)
 
-        with torch.no_grad():
-            outputs = self.runner.generate(
-                batch_input_ids,
-                max_new_tokens=2048,
-                max_attention_window_size=args.max_attention_window_size,
-                end_id=end_id,
-                pad_id=pad_id,
-                temperature=args.temperature,
-                top_k=args.top_k,
-                top_p=args.top_p,
-                num_beams=args.num_beams,
-                length_penalty=args.length_penalty,
-                early_stopping=args.early_stopping,
-                repetition_penalty=args.repetition_penalty,
-                presence_penalty=args.presence_penalty,
-                frequency_penalty=args.frequency_penalty,
-                stop_words_list=stop_words_list,
-                bad_words_list=bad_words_list,
-                output_cum_log_probs=(args.output_cum_log_probs_npy !=
-                                        None),
-                output_log_probs=(args.output_log_probs_npy != None),
-                random_seed=args.random_seed,
-                lora_uids=args.lora_task_uids,
-                prompt_table=args.prompt_table_path,
-                prompt_tasks=args.prompt_tasks,
-                streaming=args.streaming,
-                output_sequence_lengths=True,
-                return_dict=True,
-                return_all_generated_tokens=args.return_all_generated_tokens
-            )
+        try:
+            with torch.no_grad():
+                outputs = self.runner.generate(
+                    batch_input_ids,
+                    max_new_tokens=2048
+                )
+        except Exception as e:
+            metrics[common_metrics.ERROR_MSG] = str(e)
 
-            output_text = self.tokenizer.decode(outputs)
-        
-            if tensorrt_llm.mpi_rank() == 0:
-                output_ids = outputs['output_ids']
-                sequence_lengths = outputs['sequence_lengths']
-                context_logits = None
-                generation_logits = None
-                cum_log_probs = None
-                log_probs = None
-                if self.runner.gather_context_logits:
-                    context_logits = outputs['context_logits']
-                if self.runner.gather_generation_logits:
-                    generation_logits = outputs['generation_logits']
-                if args.output_cum_log_probs_npy != None:
-                    cum_log_probs = outputs['cum_log_probs']
-                if args.output_log_probs_npy != None:
-                    log_probs = outputs['log_probs']
-                print_output(self.tokenizer,
-                            output_ids,
-                            input_lengths,
-                            sequence_lengths,
-                            output_csv=args.output_csv,
-                            output_npy=args.output_npy,
-                            context_logits=context_logits,
-                            generation_logits=generation_logits,
-                            output_logits_npy=args.output_logits_npy,
-                            cum_log_probs=cum_log_probs,
-                            log_probs=log_probs,
-                            output_cum_log_probs_npy=args.output_cum_log_probs_npy,
-                            output_log_probs_npy=args.output_log_probs_npy)
+
+        output_ids = outputs['output_ids']
+        generated_text = self.tokenizer.decode(output_ids)
 
         tokens_received = len(self.tokenizer.encode(generated_text))
 
@@ -212,4 +157,5 @@ def parse_input(tokenizer,
     batch_input_ids = [
         torch.tensor(x, dtype=torch.int32) for x in batch_input_ids
     ]
+
     return batch_input_ids
